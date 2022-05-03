c7be9d2ad8bbae8c90d2436d610e867f7bf2386e
Clayton Horning <clayton.horning@colorado.edu>
2022-05-03 11:43:12 -0600

    Changed server

--- src/config/database.js
+++ src/config/database.js
@@ -1,5 +1,5 @@
-const mongoose = require('mongoose');
-require('dotenv').config({ path: `${__dirname}/.env` });
+const mongoose = require("mongoose");
+require("dotenv").config({ path: `${__dirname}/.env` });
 
 mongoose.connect(
   `mongodb+srv://${process.env.DATABASE_USERNAME}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_CLUSTER}/${process.env.COLLECTION_NAME}?retryWrites=true&w=majority`,
