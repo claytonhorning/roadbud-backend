7f0945731da8885377bb641a7357229199a02b2b
Clayton Horning <clayton.horning@colorado.edu>
2022-09-07 21:56:38 -0500

    Delete user

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,37 +1,37 @@
-const express = require('express');
+const express = require("express");
 const router = new express.Router();
-const User = require('../controllers/user.controllers');
-const auth = require('../middleware/auth');
-const access = require('../middleware/access');
+const User = require("../controllers/user.controllers");
+const auth = require("../middleware/auth");
+const access = require("../middleware/access");
 
 router.get(
-  '/user',
+  "/user",
   auth,
-  (req, res, next) => access(req, res, next, 'USER'),
+  (req, res, next) => access(req, res, next, "USER"),
   User.getUsersList
 );
 router.patch(
-  '/user/:_id',
+  "/user/:_id",
   auth,
-  (req, res, next) => access(req, res, next, 'USER'),
+  (req, res, next) => access(req, res, next, "USER"),
   User.updateUser
 );
 router.patch(
-  '/user/updateUserStatus/:_id',
+  "/user/updateUserStatus/:_id",
   auth,
-  (req, res, next) => access(req, res, next, 'USER'),
+  (req, res, next) => access(req, res, next, "USER"),
   User.updateUserStatus
 );
 router.delete(
-  '/user/:_id',
+  "/user/:_id",
   auth,
-  (req, res, next) => access(req, res, next, 'USER'),
+  // (req, res, next) => access(req, res, next, "USER"),
   User.deleteUser
 );
 router.patch(
-  '/user/activate/:_id',
+  "/user/activate/:_id",
   auth,
-  (req, res, next) => access(req, res, next, 'USER'),
+  (req, res, next) => access(req, res, next, "USER"),
   User.reActivateUserAccount
 );
 
